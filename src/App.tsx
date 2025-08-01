970160845c3661e2b024b095188cfc88802e705f
Dimasia <kovyazinaa1@gmail.com>
2025-08-01 18:23:51 +0300

    fisrt commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Dimasia </h1>
+      <button>BTN</button>
+      <a>hover anchor</a>
+    </div>
+  );
+}
+
+export default App; 
